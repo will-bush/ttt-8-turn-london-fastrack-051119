c7022b835d468b0ea0ded5c551285dc7c911198e
William Bush <williamdcbush@gmail.com>
2019-05-12 07:42:46 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -24,3 +24,20 @@ def valid_move?(board, index)
     end
   end
 
+def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
+
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    return true
+  else
+    return false
+  end
+end
+
+
+
